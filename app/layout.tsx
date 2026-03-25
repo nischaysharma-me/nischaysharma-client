1520d87a97e66ad983ad9e6a1d297a956c5b6a0e
Nischay Sharma <sharmanischay23@gmail.com>
2026-03-25 13:16:19 +0530

    Revert "feat: integrate Vercel Analytics and Speed Insights"

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,6 @@ import RealtimeNotificationHandler from "@/components/RealtimeNotificationHandle
 import SplashLoader from "@/components/SplashLoader";
 import { Dialog } from "@/components/ui/Dialog";
 import NavigationWrapper from "@/components/NavigationWrapper";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -116,8 +114,6 @@ export default function RootLayout({
         <SmoothScrollProvider>
           {children}
         </SmoothScrollProvider>
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
